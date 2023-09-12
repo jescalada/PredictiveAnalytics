e851285e756069454f919eeb1dcab1216288b4b7
Juan Escalada <juanescalada175@gmail.com>
2023-09-12 13:52:36 -0700

    Reorganize files

--- lab2/lab2.py
+++ lab2/lab2.py
@@ -5,10 +5,14 @@ colorado    = [5029196,5029316,5048281,5121771,5193721,5270482,5351218,5452107,
                5540921,5615902,5695564]
 connecticut = [3574097,3574147,3579125,3588023,3594395,3594915,3594783,3587509,
                3578674,3573880,3572665]
+delaware    = [897934,897934,899595,907316,915188,923638,932596,941413,949216,
+               957078,967171]
+
 
 # red dashes, blue squares and green triangles
 plt.plot(years, colorado, "--", color='red', label="Colorado")
-plt.plot(years,connecticut, "s", color='blue', label="Connecticut")
+plt.plot(years, connecticut, "s", color='blue', label="Connecticut")
+plt.plot(years, delaware, "-", color='green', label="Delaware")
 
 # legend
 # https://matplotlib.org/users/legend_guide.html
