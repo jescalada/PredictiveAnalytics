da5eb7ffebab2c866de934eade81e33522e46f05
Juan Escalada <juanescalada175@gmail.com>
2023-09-19 16:57:50 -0700

    Add multiple_linear_regression.py

---
+++ lab3/multiple_linear_regression1.py
@@ -0,0 +1,43 @@
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+DATASET_DIR = f"{ROOT_PATH}\\..\\datasets\\"
+CSV_DATA = "winequality.csv"
+dataset = pd.read_csv(DATASET_DIR + CSV_DATA,
+                      skiprows=1,       # Don't include header row as part of data.
+                      encoding="ISO-8859-1", sep=',',
+                      names=('fixed acidity', 'volatile acidity', 'citric acid',
+                             'residual sugar', 'chlorides', 'free sulfur dioxide',
+                             'total sulfur dioxide', 'density', 'pH', 'sulphates',
+                             'alcohol', 'quality'))
+# Show all columns.
+pd.set_option('display.max_columns', None)
+
+# Increase number of columns that display on one line.
+pd.set_option('display.width', 1000)
+print(dataset.head())
+print(dataset.describe())
+X = dataset[['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
+             'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
+             'pH', 'sulphates','alcohol']].values
+
+# Adding an intercept (required). Don't forget this step.
+# The intercept centers the error residuals around zero
+# which helps to avoid over-fitting.
+X = sm.add_constant(X)
+
+y = dataset['quality'].values
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+model = sm.OLS(y_train, X_train).fit()
+predictions = model.predict(X_test)  # make the predictions by the model
+
+print(model.summary())
+
+print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
