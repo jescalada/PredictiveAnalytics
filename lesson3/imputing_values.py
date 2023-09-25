a18c15c6b895f712374f476eb6f23fe7f10b5f38
Juan Escalada <juanescalada175@gmail.com>
2023-09-25 15:22:44 -0700

    Make code more pythonic

--- lesson3/imputing_values.py
+++ lesson3/imputing_values.py
@@ -1,12 +1,17 @@
 import pandas as pd
 import numpy  as np
+from pandas import DataFrame
 from sklearn.model_selection import train_test_split
-import statsmodels.api       as     sm
-from   sklearn               import metrics
+import statsmodels.api as sm
+from sklearn import metrics
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = f"{ROOT_PATH}\\..\\datasets\\"
 
 # Import data into a DataFrame.
-path = "/Users/pm/Desktop/DayDocs/PythonForDataAnalytics/workingData/babysamp-98.txt"
-df = pd.read_table(path, skiprows=1,
+file = "babysamp-98.txt"
+df = pd.read_table(PATH + file, skiprows=1,
                    delim_whitespace=True,
                    names=('MomAge', 'DadAge', 'MomEduc', 'MomMarital', 'numlive',
                           "dobmm", 'gestation', 'sex', 'weight', 'prenatalstart',
@@ -17,55 +22,57 @@ pd.set_option('display.width', 1000)
 print(df.head())        # View a snapshot of the data.
 print(df.describe())    # View stats including counts which highlight missing values.
 
-def convertNAcellsToNum(colName, df, measureType):
+
+def convert_nan_cells_to_num(col_name: str, dataframe: DataFrame, measure_type: str) -> DataFrame:
     # Create two new column names based on original column name.
-    indicatorColName = 'm_'   + colName # Tracks whether imputed.
-    imputedColName   = 'imp_' + colName # Stores original & imputed data.
+    indicator_col_name = 'm_' + col_name  # Tracks whether imputed.
+    imputed_col_name = 'imp_' + col_name  # Stores original & imputed data.
 
     # Get mean or median depending on preference.
-    imputedValue = 0
-    if(measureType=="median"):
-        imputedValue = df[colName].median()
-    elif(measureType=="mode"):
-        imputedValue = float(df[colName].mode())
+    imputed_value = 0
+    if measure_type == "median":
+        imputed_value = dataframe[col_name].median()
+    elif measure_type == "mode":
+        imputed_value = float(dataframe[col_name].mode())
     else:
-        imputedValue = df[colName].mean()
+        imputed_value = dataframe[col_name].mean()
 
     # Populate new columns with data.
-    imputedColumn  = []
-    indictorColumn = []
-    for i in range(len(df)):
-        isImputed = False
+    imputed_column = []
+    indicator_column = []
+    for i in range(len(dataframe)):
+        is_imputed = False
 
         # mi_OriginalName column stores imputed & original data.
-        if(np.isnan(df.loc[i][colName])):
-            isImputed = True
-            imputedColumn.append(imputedValue)
+        if np.isnan(dataframe.loc[i][col_name]):
+            is_imputed = True
+            imputed_column.append(imputed_value)
         else:
-            imputedColumn.append(df.loc[i][colName])
+            imputed_column.append(dataframe.loc[i][col_name])
 
         # mi_OriginalName column tracks if is imputed (1) or not (0).
-        if(isImputed):
-            indictorColumn.append(1)
+        if is_imputed:
+            indicator_column.append(1)
         else:
-            indictorColumn.append(0)
+            indicator_column.append(0)
 
     # Append new columns to dataframe but always keep original column.
-    df[indicatorColName] = indictorColumn
-    df[imputedColName]   = imputedColumn
-    return df
+    dataframe[indicator_col_name] = indicator_column
+    dataframe[imputed_col_name] = imputed_column
+    return dataframe
 
-df = convertNAcellsToNum('DadAge', df, "mode")
-df = convertNAcellsToNum('MomEduc', df, "mean")
-df = convertNAcellsToNum('prenatalstart', df, "median")
+
+df = convert_nan_cells_to_num('DadAge', df, "median")
+df = convert_nan_cells_to_num('MomEduc', df, "median")
+df = convert_nan_cells_to_num('prenatalstart', df, "median")
 print(df.head(10))
 
 # You can include both the indicator 'm' and imputed 'imp' columns in your model.
 # Sometimes both columns boost regression performance and sometimes they do not.
-X = df[[ 'gestation',  'm_DadAge', 'imp_DadAge', 'm_prenatalstart', 'imp_prenatalstart',
-         'm_MomEduc', 'imp_MomEduc']].values
+X = df[['gestation',  'm_DadAge', 'imp_DadAge', 'm_prenatalstart', 'imp_prenatalstart',
+        'm_MomEduc', 'imp_MomEduc']].values
 
-# Adding an intercept *** This is requried ***. Don't forget this step.
+# Adding an intercept *** This is required ***. Don't forget this step.
 X = sm.add_constant(X)
 y = df['weight'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
