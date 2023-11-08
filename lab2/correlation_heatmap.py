dea9c5aea4901cfcddee9a16a5546bb0be47b2a6
Juan Escalada <juanescalada175@gmail.com>
2023-11-07 20:49:35 -0800

    Reformat Lab 2

--- lab2/correlation_heatmap.py
+++ lab2/correlation_heatmap.py
@@ -9,18 +9,18 @@ DATASET_DIR = f"{ROOT_PATH}\\..\\datasets\\"
 # Import data into a DataFrame.
 path = f"{DATASET_DIR}bodyfat.txt"
 df = pd.read_csv(path, skiprows=1,
-                   sep='\t',
-                   names=('Density', 'Pct.BF', 'Age',   'Weight', 'Height',
-                          'Neck', 'Chest','Abdomen', 'Waist', 'Hip', 'Thigh',
-                          'Ankle', 'Knee', 'Bicep',
-                          'Forearm',
-                          'Wrist'))
+                 sep='\t',
+                 names=('Density', 'Pct.BF', 'Age', 'Weight', 'Height',
+                        'Neck', 'Chest', 'Abdomen', 'Waist', 'Hip', 'Thigh',
+                        'Ankle', 'Knee', 'Bicep',
+                        'Forearm',
+                        'Wrist'))
 # Compute the correlation matrix
 corr = df.corr()
 
 # plot the heatmap
 
-sns.set(rc = {'figure.figsize':(6,4)})
+sns.set(rc={'figure.figsize': (6, 4)})
 sns.heatmap(corr[['Weight']],
             linewidths=0.1, vmin=-1, vmax=1,
             cmap="YlGnBu")
