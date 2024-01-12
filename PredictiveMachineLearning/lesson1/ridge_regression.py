47b62f3e5588cdc5406aa1ff1163f492a95bdbc0
Juan Escalada <juanescalada175@gmail.com>
2024-01-12 10:08:10 -0800

    Add ridge_regression.py

---
+++ PredictiveMachineLearning/lesson1/ridge_regression.py
@@ -0,0 +1,92 @@
+import os
+import pandas as pd
+import numpy as np
+import statsmodels.api as sm
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import SGDRegressor
+from sklearn.linear_model import Ridge
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = f"{ROOT_PATH}\\..\\datasets\\"
+CSV_DATA = "winequality.csv"
+dataset = pd.read_csv(PATH + CSV_DATA)
+
+X = dataset[['volatile acidity', 'chlorides', 'total sulfur dioxide', 'sulphates',
+             'alcohol']]
+
+X_withConst = sm.add_constant(X)
+y = dataset['quality'].values
+
+
+# Show all columns.
+pd.set_option('display.max_columns', None)
+
+# Include only statistically significant columns.
+X = dataset[['volatile acidity', 'chlorides', 'total sulfur dioxide',
+             'pH', 'sulphates', 'alcohol']]
+X = sm.add_constant(X)
+y = dataset['quality']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+scalerX = StandardScaler()
+X_train_scaled = scalerX.fit_transform(X_train)
+
+# Build y scaler and transform y_train.
+scalerY = StandardScaler()
+y_train_scaled = scalerY.fit_transform(np.array(y_train).reshape(-1, 1))
+
+# Scale test data.
+X_test_scaled = scalerX.transform(X_test)
+
+
+def perform_linear_regression(X_train, X_test, y_train, y_test, scalerY):
+    model = sm.OLS(y_train, X_train).fit()
+    scaled_predictions = model.predict(X_test)  # make the predictions by the model
+    predictions = scalerY.inverse_transform(np.array(scaled_predictions).reshape(-1, 1))
+    print(model.summary())
+    print('Root Mean Squared Error:',
+          np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+    return predictions
+
+
+predictions = perform_linear_regression(X_train_scaled, X_test_scaled,
+                                        y_train_scaled, y_test, scalerY)
+
+def perform_SGD(X_train, X_test, y_train, y_test, scalerY):
+    sgd = SGDRegressor()
+    sgd.fit(X_train, y_train)
+    print("\n***SGD=")
+    predictions_unscaled = sgd.predict(X_test)
+    predictions = scalerY.inverse_transform(
+        np.array(predictions_unscaled).reshape(-1,1))
+
+    print('Root Mean Squared Error:',
+          np.sqrt(metrics.mean_squared_error(y_test,
+                                             predictions)))
+
+
+perform_SGD(X_train_scaled, X_test_scaled,
+            y_train_scaled, y_test, scalerY)
+
+def ridge_regression(X_train, X_test, y_train, y_test, alpha, scalerY):
+    # Fit the model
+    ridgereg = Ridge(alpha=alpha)
+    ridgereg.fit(X_train, y_train)
+    y_pred_scaled = ridgereg.predict(X_test)
+
+   # predictions = scalerY.inverse_transform(y_pred.reshape(-1,1))
+    print("\n***Ridge Regression Coefficients ** alpha=" + str(alpha))
+    print(ridgereg.intercept_)
+    print(ridgereg.coef_)
+    y_pred = scalerY.inverse_transform(np.array(y_pred_scaled).reshape(-1,1))
+    print('Root Mean Squared Error:',
+          np.sqrt(metrics.mean_squared_error(y_test, y_pred)))
+X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                         test_size=0.2, random_state=0)
+alphaValues = [0,  0.16, 0.17, 0.18]
+for i in range(0, len(alphaValues)):
+    ridge_regression(X_train_scaled, X_test_scaled, y_train_scaled,
+                     y_test, alphaValues[i], scalerY)
