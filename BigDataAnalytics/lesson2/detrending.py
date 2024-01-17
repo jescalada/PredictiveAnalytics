55736585bd83b473a36fb796143e3ce1e345087a
Juan Escalada <juanescalada175@gmail.com>
2024-01-17 11:21:58 -0800

    Add detrending.py

---
+++ BigDataAnalytics/lesson2/detrending.py
@@ -0,0 +1,20 @@
+from statsmodels.tsa.seasonal import seasonal_decompose
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = f"{ROOT_PATH}\\..\\datasets\\"
+FILE = "drugSales.csv"
+df = pd.read_csv(PATH + FILE, parse_dates=['date'], index_col='date')
+tseries = seasonal_decompose(df['value'], model='additive',
+                             extrapolate_trend='freq')
+
+plt.plot(df['value'])
+plt.title("Drug Sales", fontsize=16)
+plt.show()
+
+detrended = df['value'] - tseries.trend
+plt.plot(detrended)
+plt.title('Drug Sales After Subtracting Trend', fontsize=16)
+plt.show()
