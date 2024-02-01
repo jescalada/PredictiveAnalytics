59e71859c8ad7b05d11fd19fe866ac663a7e8adb
Juan Escalada <juanescalada175@gmail.com>
2024-02-01 15:29:51 -0800

    Add seasonal_differencing.py

---
+++ BigDataAnalytics/lab4/seasonal_differencing.py
@@ -0,0 +1,29 @@
+import warnings
+
+warnings.filterwarnings("ignore")
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
+PATH = f"{ROOT_PATH}\\..\\datasets\\"
+FILE_NAME = "drugSales.csv"
+
+# Import
+data = pd.read_csv(PATH + FILE_NAME, parse_dates=['date'], index_col='date')
+
+# Plot
+fig, axes = plt.subplots(2, 1, figsize=(10, 5), dpi=100, sharex=True)
+
+# Usual Differencing
+axes[0].plot(data[:], label='Original Series')
+axes[0].plot(data[:].diff(1), label='Usual Differencing')
+axes[0].set_title('Usual Differencing')
+axes[0].legend(loc='upper left', fontsize=10)
+
+# Seasonal Differencing
+axes[1].plot(data[:], label='Original Series')
+axes[1].plot(data[:].diff(12), label='Seasonal Differencing', color='green')
+axes[1].set_title('Seasonal Differencing')
+plt.legend(loc='upper left', fontsize=10)
+plt.show()
